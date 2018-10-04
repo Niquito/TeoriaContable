83d34f5aafc1088d9935ab3cf1224bf80b9a86bc
Niquito <ncaffarini@gmail.com>
2018-10-04 11:12:27 -0300

    Pasado Vue a versiòn de prod. Terminado c3s1e5

---
+++ src/js/casosAleatorios_texto.js
@@ -0,0 +1,48 @@
+function traerCasoAleatorio(){
+
+    var obj_keys = Object.keys(enunciados);
+    var k =  obj_keys[Math.floor(Math.random() *obj_keys.length)];
+    return enunciados[k];
+
+}
+
+new Vue({
+  el: '#app',
+  data () {
+        return {
+          i:0,
+          descripcion:'',
+          casos: []
+        }
+      },
+      methods:{
+        siguienteCaso(){
+
+            var error = false;
+            var inputs = document.getElementsByTagName('input');
+            for(var i = 0; i < inputs.length; i++) {
+            	if (inputs[i].value.toLowerCase().trim() != inputs[i].dataset.respuesta.toLowerCase().trim()){
+            		error = true;
+            	}
+            }
+
+            if (!error){
+            	const caso = traerCasoAleatorio();
+                this.i++;
+                this.casos.push(caso);
+            } else {
+                var tabla = document.getElementsByTagName('table');//
+                tabla[0].classList.add("animar-error");
+
+                tabla[0].addEventListener("animationend", (e) => {
+                    tabla[0].classList.remove("animar-error");
+                });
+            }
+            error = false;
+        },
+        removerCaso(caso) {
+            this.casos.splice(this.casos.indexOf(caso),1);
+            this.i--;
+        }
+      }
+})
